955ac80cac093743c89d4f1c2a8bf8ef67ae8cd3
vlad1878 <103626199+vlad1878@users.noreply.github.com>
2022-08-24 19:21:05 +0200

    Add led using pwm to control state of mikrocontroller

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -58,11 +58,15 @@ DMA_HandleTypeDef hdma_adc1;
 
 I2C_HandleTypeDef hi2c1;
 
+TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim3;
 
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+unsigned long t_led = 0;
+bool led_direction = 0;
+int8_t led_pwm = 0;
 extern uint16_t ADC_SMA_Data[5];
 extern bool adc_flag;
 extern char tx_buffer_lcd[40];
@@ -83,12 +87,14 @@ static void MX_USART2_UART_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_TIM3_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
 void print_adc(void);
 float adc_to_volt(uint16_t adc_val);
 float conversation_adc_to_val(float x, float in_min, float in_max, float out_min, float out_max);
 void select_lcd_mode(void);
 float vRef_volt_adc(void);
+void led_work(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -129,9 +135,12 @@ int main(void)
   MX_ADC1_Init();
   MX_I2C1_Init();
   MX_TIM3_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
+  t_led = HAL_GetTick();
   HAL_Delay(100);
   HAL_TIM_Base_Start_IT(&htim3);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
   while(!lcd_init_flag){
 	  lcd1602_SetCursor(1, 0);
 	  sprintf(tx_buffer_lcd, "Initialization..");
@@ -153,6 +162,7 @@ int main(void)
   {
 	  select_lcd_mode();
 	  print_adc();
+	  led_work();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -322,6 +332,55 @@ static void MX_I2C1_Init(void)
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 71;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 100;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 50;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+  HAL_TIM_MspPostInit(&htim2);
 
 }
 
@@ -548,6 +607,26 @@ void select_lcd_mode(){
 
 float vRef_volt_adc(){
 	return mkRefVcc / ADC_SMA_Data[4];
+}
+
+void led_work(){
+	if(HAL_GetTick() - t_led > 30){
+		t_led = HAL_GetTick();
+		if(led_direction){
+			led_pwm += 1;
+			if(led_pwm >= 100){
+				led_direction = 0;
+			}
+		}
+		else if(!led_direction){
+			led_pwm -= 1;
+			if(led_pwm <= 0){
+				led_direction = 1;
+			}
+		}
+		TIM2->CCR1 = led_pwm;
+
+	}
 }
 /* USER CODE END 4 */
 
